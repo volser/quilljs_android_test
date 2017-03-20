7ab496e4c5452e3cbb038874e8bfbb6d013a1205
volser <volser@apec.com.ua>
2017-03-20 11:52:34 +0200

    init

---
+++ app/src/main/java/com/quilljs/android/MainActivity.java
@@ -0,0 +1,28 @@
+package com.quilljs.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String SETUP_HTML = "file:///android_asset/editor.html";
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.setVerticalScrollBarEnabled(false);
+        mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl(SETUP_HTML);
+    }
+}
